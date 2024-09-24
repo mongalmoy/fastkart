f5162e3099b4fba2cfc1e5f0dd0f19ab0c46d0d2
Mongalmoy Karmakar <mongalmoykarmakar.bwn@gmail.com>
2024-09-24 23:52:57 +0530

    wip

--- src/react-redux/provider.js
+++ src/react-redux/provider.js
@@ -1,10 +1,8 @@
 import { Provider } from "react-redux";
-import { store } from "./store";
+// import { store } from "./store";
 
 export function Providers({children}) {
   return (
-    <Provider store={store}>
-      {children}
-    </Provider>
+    {children}
   )
 }
