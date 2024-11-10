4af44d9fa298788dd8f52585e8b996532274ad12
Mongalmoy Karmakar <mongalmoykarmakar.bwn@gmail.com>
2024-11-10 23:39:08 +0530

    ssl false added for prod

--- src/lib/postgres.js
+++ src/lib/postgres.js
@@ -1,12 +1,23 @@
 import { Pool } from "pg";
 
-const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: Number(process.env.PG_PORT),
-  // ssl: { rejectUnauthorized: false }, // Important for Aiven connections
-});
+const pool = new Pool(
+  process.env.PROD_ENV === "false"
+    ? {
+        user: process.env.PG_USER,
+        host: process.env.PG_HOST,
+        database: process.env.PG_DATABASE,
+        password: process.env.PG_PASSWORD,
+        port: Number(process.env.PG_PORT),
+        // ssl: { rejectUnauthorized: false }, // Important for Aiven connections
+      }
+    : {
+        user: process.env.PG_USER,
+        host: process.env.PG_HOST,
+        database: process.env.PG_DATABASE,
+        password: process.env.PG_PASSWORD,
+        port: Number(process.env.PG_PORT),
+        ssl: { rejectUnauthorized: false }, // Important for Aiven connections
+      }
+);
 
 export default pool;
